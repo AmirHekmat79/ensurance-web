dc4159e72705e246c3eb896a4ff170f4a288c713
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-06-26 11:56:02 +0330

    Add bar-line

---
+++ src/components/Header/bar-navigation.vue
@@ -0,0 +1,80 @@
+<template>
+ <q-bar class="bar-bg text-info  q-py-lg">
+   <div class="row justify-start items-center text-center">
+    <div class="contact-container">
+        <a class="cursor-pointer">
+            <span class="bar-link">درباره ما</span>
+            <q-img class="q-mx-sm" width="16px" src="/icons/letter.svg"/>
+           
+        </a>
+        <a class="cursor-pointer" >
+            <span class="bar-link">تماس با ما</span>    
+            <q-img  class="q-mx-sm" width="20px" src="/icons/info_icon.svg"/>
+        </a>
+    </div>
+   
+    <div class="q-gutter-sm social-svg-container">
+        <a class="cursor-pointer">
+        <q-img width="20px" src="/icons/whatsapp.svg"/>
+       </a>
+    <a class="cursor-pointer">
+        <q-img width="20px" src="/icons/twitter-x.svg"/>
+    </a>
+    <a class="cursor-pointer">
+        <q-img  width="20px"  src="/icons/telegram.svg"/>
+    </a>
+    <a class="cursor-pointer">
+        <q-img   width="20px" src="/icons/robika.svg"/>
+    </a>
+    <a class="cursor-pointer">
+        <q-img  width="20px"  src="/icons/linkedin.svg"/>
+    </a>
+    <a class="cursor-pointer">
+        <q-img   width="20px" src="/icons/instagram.svg"/>
+    </a>
+    <a class="cursor-pointer">
+        <q-img   width="20px" src="/icons/facebook.svg"/>
+    </a>
+    <a class="cursor-pointer">
+        <q-img   width="20px"  src="/icons/eitaa.svg"/>
+    </a>
+    <a class="cursor-pointer"><q-img width="20px" src="/icons/bale.svg"/></a>  
+    </div>
+   
+   </div>
+ </q-bar>
+</template>
+  
+<script>
+  import { defineComponent } from 'vue'
+  
+  
+  export default defineComponent({
+    name: 'BarNavigation',
+  
+  })
+</script>
+<style scoped>
+.social-svg-container{
+  position: absolute;
+  right: 8px;
+  padding-right: 130px;
+}
+.bar-bg{
+    background-color: #319ad6 !important;
+   
+}
+.bar-link{
+    color: #8ee8d7 !important;
+}
+.contact-container{
+    padding-left: 130px;
+}
+@media only screen and (max-width: 640px) {
+    .contact-container{
+        display: none;
+    }
+}
+</style>
+  
+  
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <BarNavigation/>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import BarNavigation from '../components/Header/bar-navigation.vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    BarNavigation
+  },
+
+  
+})
+</script>
