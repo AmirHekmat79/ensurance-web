3296182dd77fe68d3bde31427d9f679f53ab853c
AmirHekmat79 <amir.hekmat1379@gmail.com>
2024-07-12 16:14:24 +0330

    Add Navbar Style Changes

--- src/components/Header/toolbar-navigation.vue
+++ src/components/Header/toolbar-navigation.vue
@@ -1,32 +1,74 @@
 <template>
-  <q-toolbar class="text-primary shadow-6 column justify-start items-center q-py-md">
-    <div class="row justify-between items-center">
-      <q-btn @click="toggleSidebar" flat class="text-white hambergur-menu" icon="menu"></q-btn>
-      <div class="login-btn-group justify-between items-center ">
+  <q-toolbar
+    class="text-primary shadow-6 column justify-start items-center q-py-md"
+  >
+    <div
+      class="row justify-between items-center"
+      style="padding-top: 10px !important"
+    >
+      <q-btn
+        @click="toggleSidebar"
+        flat
+        class="text-white hambergur-menu"
+        icon="menu"
+      ></q-btn>
+      <div class="login-btn-group justify-between items-center">
         <q-btn class="register-btn"><a>ثبت نام</a></q-btn>
         <q-btn class="entrance-btn"><a>ورود</a></q-btn>
       </div>
-      <q-toolbar-title  class="text-white title-container  q-ml-auto">
+      <q-toolbar-title class="text-white title-container q-ml-auto">
         <h6 class="title">{{ InsuranceHeaderInfo?.centreName }}</h6>
       </q-toolbar-title>
     </div>
     <div class="row justify-start items-center q-ml-auto">
-      <q-list v-if="!sidebarVisible" class="navLink-container row justify-start items-center">
-          <q-item v-for="(item, index) in sortedMenuItems"  :key="index"   class="navLink-item">
-            <submenu class="navLink-btn" :isSubMenu="hasChildItems(item.id)" :menus="getChildItems(item.id)" :menu="item" :allMenu = "InsuranceNavbarMenuItems" isFirst/>
-          </q-item>
-          
-  
+      <q-list
+        v-if="!sidebarVisible"
+        class="navLink-container row justify-start items-center"
+      >
+        <q-item
+          v-for="(item, index) in sortedMenuItems"
+          :key="index"
+          class="navLink-item"
+        >
+          <submenu
+            class="navLink-btn"
+            :isSubMenu="hasChildItems(item.id)"
+            :menus="getChildItems(item.id)"
+            :menu="item"
+            :allMenu="InsuranceNavbarMenuItems"
+            isFirst
+          />
+        </q-item>
       </q-list>
-      <q-drawer v-else v-model="sidebarVisible" side="left" show-if-above class="q-py-md" :style="{ width: '200px' }">
+      <q-drawer
+        v-else
+        v-model="sidebarVisible"
+        side="left"
+        show-if-above
+        class="q-py-md"
+        :style="{ width: '200px' }"
+      >
         <q-separator class="q-my-xl" color="gray-blue-10" inset />
-        <div class="column justify-center items-center sidebar-login-btn-container">
+        <div
+          class="column justify-center items-center sidebar-login-btn-container"
+        >
           <q-btn class="register-btn btn-fixed-width"><a>ثبت نام</a></q-btn>
           <q-btn class="entrance-btn btn-fixed-width"><a>ورود</a></q-btn>
         </div>
         <q-list class="q-mt-md">
-          <q-item v-for="(item, index) in sortedMenuItems.reverse()"  :key="index"   class="navLink-item">
-            <submenu class="navLink-btn" :isSubMenu="hasChildItems(item.id)" :menus="getChildItems(item.id)" :menu="item" :allMenu = "InsuranceNavbarMenuItems" isFirst/>
+          <q-item
+            v-for="(item, index) in sortedMenuItems.reverse()"
+            :key="index"
+            class="navLink-item"
+          >
+            <submenu
+              class="navLink-btn"
+              :isSubMenu="hasChildItems(item.id)"
+              :menus="getChildItems(item.id)"
+              :menu="item"
+              :allMenu="InsuranceNavbarMenuItems"
+              isFirst
+            />
           </q-item>
         </q-list>
       </q-drawer>
@@ -34,65 +76,68 @@
   </q-toolbar>
 </template>
 
-
 <script>
 import { defineComponent, ref } from "vue";
 import apiService from "src/services/api-services";
-import submenu from 'src/components/Header/submenu.vue';
+import submenu from "src/components/Header/submenu.vue";
 export default defineComponent({
   name: "ToolbarNavigation",
-  components:{
-    submenu
+  components: {
+    submenu,
   },
   data() {
-      return {
-        InsuranceHeaderInfo: null,
-        InsuranceNavbarMenuItems: [],
-      };
-    },
-  
-    mounted() {
-      this.getPolicyIntroduction();
-      this.getMenuesItem();
-    },
-  
-    methods: {
-      getPolicyIntroduction() {
-        apiService
-          .getPolicyIntroduction('sabz')
-          .then((response) => {
-            this.InsuranceHeaderInfo = response.data.message?.insuranceCentrePortal;
-            
-          })
-          .catch((error) => {
-            console.error('Error fetching insurance centre info:', error);
-          });
-      },
-      getMenuesItem() {
-        apiService
-          .getMenuesItem('sabz')
-          .then((response) => {
-            this.InsuranceNavbarMenuItems = response.data.message;
+    return {
+      InsuranceHeaderInfo: null,
+      InsuranceNavbarMenuItems: [],
+    };
+  },
 
-          })
-          .catch((error) => {
-            console.error('Error fetching insurance centre info:', error);
-          });
-      },
-      hasChildItems(itemId){
-          return this.InsuranceNavbarMenuItems.filter(item=> item.parentId == itemId).length > 0;
-        
-      } ,
-      getChildItems(itemId){
-        return this.InsuranceNavbarMenuItems.filter((item) => item.parentId == itemId)
-      }
+  mounted() {
+    this.getPolicyIntroduction();
+    this.getMenuesItem();
+  },
+
+  methods: {
+    getPolicyIntroduction() {
+      apiService
+        .getPolicyIntroduction("sabz")
+        .then((response) => {
+          this.InsuranceHeaderInfo =
+            response.data.message?.insuranceCentrePortal;
+        })
+        .catch((error) => {
+          console.error("Error fetching insurance centre info:", error);
+        });
     },
-    computed: {
-      sortedMenuItems() {
-      return this.InsuranceNavbarMenuItems.filter(item => item.parentId == null).sort((a, b) => b.order - a.order)
-    
-    }
-},
+    getMenuesItem() {
+      apiService
+        .getMenuesItem("sabz")
+        .then((response) => {
+          this.InsuranceNavbarMenuItems = response.data.message;
+        })
+        .catch((error) => {
+          console.error("Error fetching insurance centre info:", error);
+        });
+    },
+    hasChildItems(itemId) {
+      return (
+        this.InsuranceNavbarMenuItems.filter((item) => item.parentId == itemId)
+          .length > 0
+      );
+    },
+    getChildItems(itemId) {
+      return this.InsuranceNavbarMenuItems.filter(
+        (item) => item.parentId == itemId
+      );
+    },
+  },
+  computed: {
+    sortedMenuItems() {
+      return this.InsuranceNavbarMenuItems.filter(
+        (item) => item.parentId == null
+      ).sort((a, b) => b.order - a.order);
+    },
+  },
   setup() {
     const sidebarVisible = ref(false);
     function toggleSidebar() {
@@ -108,18 +153,17 @@ export default defineComponent({
 </script>
 <style scoped>
 .title-container {
-  margin-left : 440px !important;
+  margin-left: 440px !important;
   text-align: right !important;
 }
 .title {
   font-size: 17px;
-  white-space:nowrap; 
-  width:100%;
+  white-space: nowrap;
+  width: 100%;
   margin-left: auto;
   font-weight: 500;
-  
 }
-*{
+* {
   padding: 0 !important;
 }
 .entrance-btn {
@@ -215,8 +259,8 @@ export default defineComponent({
   }
   .title-container {
     margin-left: 300px !important;
-  text-align: right !important;
-}
+    text-align: right !important;
+  }
   .login-btn-group {
     display: none;
   }
@@ -233,22 +277,21 @@ export default defineComponent({
   .sidebar-navLink-container {
     display: block;
   }
-  .register-btn , .entrance-btn {
-    min-width:230px;
+  .register-btn,
+  .entrance-btn {
+    min-width: 230px;
     width: 60%;
     text-align: center;
     margin: 6px auto !important;
   }
 }
 @media only screen and (max-width: 727px) {
-
   .hambergur-menu {
     display: block;
     /* margin-left: 65px; */
   }
-  .title-container{
+  .title-container {
     margin-left: 200px !important;
   }
-
 }
 </style>
